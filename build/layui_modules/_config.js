33557b13acf6ac5dc548acae47576b47d45b0896
D.Xuffy <d.xuffy@qq.com>
2017-05-19 15:16:15 +0800

    d.xuffy 重构
    d.xuffy 重构

---
+++ build/layui_modules/_config.js
@@ -0,0 +1,1 @@
+"use strict";layui.define(function(o){var i={};i.env={develop:{api:"http://192.168.0.200:8080/labpoo/",imageUrl:"http://192.168.0.200:8080/labpoo/system/showPic/"},production:{api:"/labpoo/",imageUrl:"/labpoo/system/showPic/"}}["production"],o("_config",i)});
